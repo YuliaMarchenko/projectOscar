be87500a0d823f16cf84ddc7f5516b25bb6bf54c
Yulia Marchenko <yuyakovleva11@gmail.com>
2022-12-01 10:32:36 +0100

    first commit

---
+++ src/test/java/com/telran/oscar/tests/register/registerTests.java
@@ -0,0 +1,10 @@
+package com.telran.oscar.tests.register;
+
+import com.telran.oscar.tests.TestBase;
+import org.testng.annotations.Test;
+
+public class registerTests extends TestBase {
+    @Test
+    public void open(){
+    }
+}
